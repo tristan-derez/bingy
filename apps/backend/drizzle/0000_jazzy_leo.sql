8d631017e84530e604709a3c338416b4db229fb6
Tristan <tristan.derez@gmail.com>
2025-09-16 22:15:42 +0200

    fix(back): recreate migrations

--- apps/backend/drizzle/0000_jazzy_leo.sql
+++ apps/backend/drizzle/0000_jazzy_leo.sql
@@ -15,6 +15,15 @@ CREATE TABLE "accounts" (
 	"deleted_at" timestamp
 );
 --> statement-breakpoint
+CREATE TABLE "codes" (
+	"user_id" uuid NOT NULL,
+	"code" varchar(6) NOT NULL,
+	"expires_at" timestamp with time zone NOT NULL,
+	"updated_at" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp
+);
+--> statement-breakpoint
 CREATE TABLE "sessions" (
 	"id" uuid PRIMARY KEY DEFAULT uuidv7() NOT NULL,
 	"user_id" uuid NOT NULL,
@@ -34,6 +43,20 @@ CREATE TABLE "two_factor" (
 	"backup_codes" text
 );
 --> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT uuidv7() NOT NULL,
+	"name" varchar(256),
+	"email" varchar(256) NOT NULL,
+	"avatar_url" text,
+	"email_verified" boolean DEFAULT false NOT NULL,
+	"email_verified_at" timestamp,
+	"two_factor_enabled" boolean DEFAULT false NOT NULL,
+	"updated_at" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
 CREATE TABLE "verifications" (
 	"id" uuid PRIMARY KEY DEFAULT uuidv7() NOT NULL,
 	"identifier" text,
@@ -44,22 +67,12 @@ CREATE TABLE "verifications" (
 	"deleted_at" timestamp
 );
 --> statement-breakpoint
-ALTER TABLE "oauth_accounts" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-DROP TABLE "oauth_accounts" CASCADE;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "id" SET DATA TYPE uuid;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "id" SET DEFAULT uuidv7();--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "id" DROP IDENTITY;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "email_verified" SET DEFAULT false;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "email_verified" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "two_factor_enabled" boolean DEFAULT false NOT NULL;--> statement-breakpoint
 ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "codes" ADD CONSTRAINT "codes_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "two_factor" ADD CONSTRAINT "two_factor_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE UNIQUE INDEX "unique_user_provider" ON "accounts" USING btree ("user_id","provider_id");--> statement-breakpoint
 CREATE UNIQUE INDEX "unique_provider_account" ON "accounts" USING btree ("provider_id","account_id");--> statement-breakpoint
 CREATE INDEX "idx_accounts_provider" ON "accounts" USING btree ("provider_id");--> statement-breakpoint
-CREATE UNIQUE INDEX "unique_session_token" ON "sessions" USING btree ("token");--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "password";--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "two_factor_method";--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "two_factor_enabled_at";--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "last_login_at";
+CREATE UNIQUE INDEX "unique_code_user" ON "codes" USING btree ("user_id");--> statement-breakpoint
+CREATE UNIQUE INDEX "unique_session_token" ON "sessions" USING btree ("token");
