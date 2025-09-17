35297ae64aa524c61bd4b3992fd5e48b47c9348e
Tristan <tristan.derez@gmail.com>
2025-09-17 16:35:24 +0200

    delete useless comp

--- apps/frontend/src/routes/welcome.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/welcome")({
-	component: WelcomePage,
-});
-
-function WelcomePage() {
-	return <div>Hello "/welcome"!</div>;
-}
