44089d375c202a0b5c6877f2d64035b94f31bcf3
Tristan <tristan.derez@gmail.com>
2025-09-29 18:15:54 +0200

    fix(front): replace "/sign-in" with "/"

--- apps/frontend/src/routes/goodbye.tsx
+++ apps/frontend/src/routes/goodbye.tsx
@@ -11,9 +11,7 @@ function GoodByePage() {
 		<div className="min-h-screen flex flex-col items-center justify-center">
 			<div className="grid gap-4 text-center">
 				<p>Hey! goodbye!</p>
-				<Button onClick={() => navigate({ to: "/signin" })}>
-					Return to home
-				</Button>
+				<Button onClick={() => navigate({ to: "/" })}>Return to home</Button>
 			</div>
 		</div>
 	);
