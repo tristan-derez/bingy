3ab32479c48eae9dabf34f85762637f2779a513a
Tristan <tristan.derez@gmail.com>
2025-09-30 11:41:52 +0200

    refactor(front): change name from settings to account

--- apps/frontend/src/routes/_auth.account.tsx
+++ apps/frontend/src/routes/_auth.account.tsx
@@ -1,11 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { AccountComponent } from "@/components/auth/account-component";
 
-export const Route = createFileRoute("/_auth/settings")({
-	component: SettingsPage,
+export const Route = createFileRoute("/_auth/account")({
+	component: AccountPage,
 });
 
-function SettingsPage() {
+function AccountPage() {
 	return (
 		<div className="min-h-svh flex flex-col items-center justify-center px-4 space-y-6">
 			<AccountComponent />
