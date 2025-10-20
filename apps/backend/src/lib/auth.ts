cb7b41130ca77698e4d126a2f7d802f7a2acf228
Tristan <tristan.derez@gmail.com>
2025-10-20 16:40:21 +0200

    feat: add custom cookie name

--- apps/backend/src/lib/auth.ts
+++ apps/backend/src/lib/auth.ts
@@ -185,6 +185,7 @@ export const auth = betterAuth({
 		}),
 		lastLoginMethod({
 			storeInDatabase: true,
+			cookieName: "bingy.last_used_login_method",
 		}),
 	],
 });
--- apps/frontend/src/lib/auth-client.ts
+++ apps/frontend/src/lib/auth-client.ts
@@ -3,4 +3,9 @@ import { config } from "./env";
 
 export const authClient = createAuthClient({
 	baseURL: config.apiUrl,
+	plugins: [
+		lastLoginMethodClient({
+			cookieName: "bingy.last_used_login_method",
+		}),
+	],
 });
