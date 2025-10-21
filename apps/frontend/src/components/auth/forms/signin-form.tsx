f26c44bc84382297bb59d9d4752017d3a820e572
Tristan <tristan.derez@gmail.com>
2025-10-21 02:34:00 +0200

    feat(front): add dialog if 2fa is enabled at sign in

--- apps/frontend/src/components/auth/forms/signin-form.tsx
+++ apps/frontend/src/components/auth/forms/signin-form.tsx
@@ -1,7 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Link, useNavigate } from "@tanstack/react-router";
 import { Loader2 } from "lucide-react";
-import React, { useId } from "react";
+import { useId, useState } from "react";
 import { type SubmitHandler, useForm } from "react-hook-form";
 import { FcGoogle } from "react-icons/fc";
 import { toast } from "sonner";
@@ -30,9 +30,11 @@ import { SeparatorWithText } from "@/components/ui/separator-text";
 import { authClient } from "@/lib/auth-client";
 import { config } from "@/lib/env";
 import { signinFormSchema } from "@/schemas/signin-form-schema";
+import { TwoFactorDialog } from "../two-factor.dialog";
 
 export function SignInForm() {
-	const [isSubmitting, setIsSubmitting] = React.useState(false);
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	const [showDialog, setShowDialog] = useState(false);
 	const id = useId();
 	const navigate = useNavigate();
 	const lastMethod = authClient.getLastUsedLoginMethod();
@@ -52,21 +54,29 @@ export function SignInForm() {
 	) => {
 		setIsSubmitting(true);
 		try {
-			const { data, error } = await authClient.signIn.email({
-				email: formData.email,
-				password: formData.password,
-			});
-
-			if (error) {
-				toast.error(error.message || "Oops! Request failed, try again.");
-			}
-
-			if (data?.user.emailVerified === false) {
-				toast.success(`Please verify your email at ${data.user.email}`);
-				navigate({ to: "/welcome" });
-			} else if (data?.user.emailVerified) {
-				navigate({ to: "/dashboard" });
-			}
+			await authClient.signIn.email(
+				{
+					email: formData.email,
+					password: formData.password,
+				},
+				{
+					async onSuccess(context) {
+						if (context.data.twoFactorRedirect) {
+							setShowDialog(true);
+						} else if (context.data.user.emailVerified === "false") {
+							toast.success("Please verify your email");
+							navigate({ to: "/welcome" });
+						} else if (context.data.user.emailVerified) {
+							navigate({ to: "/dashboard" });
+						}
+					},
+					async onError(context) {
+						toast.error(
+							context.error.message || "Oops! Request failed, try again.",
+						);
+					},
+				},
+			);
 		} catch (err) {
 			const message =
 				err instanceof Error
@@ -98,117 +108,154 @@ export function SignInForm() {
 		}
 	};
 
+	const handleVerifyTotp = async (code: string) => {
+		try {
+			const { data, error } = await authClient.twoFactor.verifyTotp({
+				code,
+				trustDevice: true,
+			});
+
+			if (error?.message === "Invalid two factor cookie") {
+				toast.error("Invalid code. Try again.");
+				return;
+			} else if (error) {
+				toast.error(error.message);
+				return;
+			}
+
+			if (data) {
+				toast.success(`Welcome back, ${data.user.name}!`);
+				setShowDialog(false);
+				form.reset();
+				navigate({ to: "/dashboard" });
+			}
+		} catch (err) {
+			const message =
+				err instanceof Error
+					? err.message
+					: "Failed to verify code. Try again.";
+			toast.error(message);
+		}
+	};
+
 	return (
-		<Card className="mx-auto max-w-sm min-w-[320px] md:min-w-[420px]">
-			<CardHeader>
-				<CardTitle className="text-2xl">Sign in</CardTitle>
-				<CardDescription>
-					<p>
-						Welcome back! Your entertainment hub is waiting. <br />
-						Just sign in to get going.
-					</p>
-				</CardDescription>
-			</CardHeader>
-			<CardContent className="grid gap-4">
-				<Form {...form}>
-					<form
-						onSubmit={form.handleSubmit(onFormSubmit)}
-						className="grid gap-4"
-					>
-						<fieldset disabled={isSubmitting}>
-							<div className="grid gap-4">
-								<FormField
-									control={form.control}
-									name="email"
-									render={({ field }) => (
-										<FormItem className="grid gap-2">
-											<FormLabel htmlFor={`${id}-email`}>Email</FormLabel>
-											<FormControl>
+		<>
+			<Card className="mx-auto max-w-sm min-w-[320px] md:min-w-[420px]">
+				<CardHeader>
+					<CardTitle className="text-2xl">Sign in</CardTitle>
+					<CardDescription>
+						<p>
+							Welcome back! Your entertainment hub is waiting. <br />
+							Just sign in to get going.
+						</p>
+					</CardDescription>
+				</CardHeader>
+				<CardContent className="grid gap-4">
+					<Form {...form}>
+						<form
+							onSubmit={form.handleSubmit(onFormSubmit)}
+							className="grid gap-4"
+						>
+							<fieldset disabled={isSubmitting}>
+								<div className="grid gap-4">
+									<FormField
+										control={form.control}
+										name="email"
+										render={({ field }) => (
+											<FormItem className="grid gap-2">
+												<FormLabel htmlFor={`${id}-email`}>Email</FormLabel>
+												<FormControl>
+													<Input
+														id={`${id}-email`}
+														type="email"
+														autoComplete="email"
+														placeholder="m@example.com"
+														required
+														{...field}
+													/>
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+									<FormField
+										control={form.control}
+										name="password"
+										render={({ field }) => (
+											<FormItem className="grid gap-2">
+												<div className="flex items-center">
+													<FormLabel htmlFor={`${id}-password`}>
+														Password
+													</FormLabel>
+													<Link
+														to={"/forgot-password"}
+														search={
+															watchedEmail ? { email: watchedEmail } : undefined
+														}
+														className="ml-auto inline-block text-xs underline"
+													>
+														Forgot your password?
+													</Link>
+												</div>
 												<Input
-													id={`${id}-email`}
-													type="email"
-													autoComplete="email"
-													placeholder="m@example.com"
-													required
+													id={`${id}-password`}
+													type="password"
+													autoComplete="current-password"
 													{...field}
 												/>
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								<FormField
-									control={form.control}
-									name="password"
-									render={({ field }) => (
-										<FormItem className="grid gap-2">
-											<div className="flex items-center">
-												<FormLabel htmlFor={`${id}-password`}>
-													Password
-												</FormLabel>
-												<Link
-													to={"/forgot-password"}
-													search={
-														watchedEmail ? { email: watchedEmail } : undefined
-													}
-													className="ml-auto inline-block text-xs underline"
-												>
-													Forgot your password?
-												</Link>
-											</div>
-											<Input
-												id={`${id}-password`}
-												type="password"
-												autoComplete="current-password"
-												{...field}
-											/>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								<Button
-									type="submit"
-									className="w-full mt-2 font-bold flex justify-center relative disabled:bg-gray-300 disabled:text-gray-500 hover:cursor-pointer"
-								>
-									{isSubmitting ? (
-										<span className="flex items-center justify-center gap-2">
-											<Loader2 className="animate-spin h-4 w-4" />
-											Signing in
-										</span>
-									) : (
-										"Sign in"
-									)}
-									{lastMethod === "email" && (
-										<Badge
-											variant="secondary"
-											className="absolute right-2 rounded-md"
-										>
-											Last used
-										</Badge>
-									)}
-								</Button>
-							</div>
-						</fieldset>
-					</form>
-				</Form>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+									<Button
+										type="submit"
+										className="w-full mt-2 font-bold flex justify-center relative disabled:bg-gray-300 disabled:text-gray-500 hover:cursor-pointer"
+									>
+										{isSubmitting ? (
+											<span className="flex items-center justify-center gap-2">
+												<Loader2 className="animate-spin h-4 w-4" />
+												Signing in
+											</span>
+										) : (
+											"Sign in"
+										)}
+										{lastMethod === "email" && (
+											<Badge
+												variant="secondary"
+												className="absolute right-2 rounded-md"
+											>
+												Last used
+											</Badge>
+										)}
+									</Button>
+								</div>
+							</fieldset>
+						</form>
+					</Form>
 
-				<SeparatorWithText text="Or continue with" />
-				<div className="flex gap-2">
-					<OAuthButton
-						icon={FcGoogle}
-						label="Sign in with Google"
-						text="Google"
-						lastMethod={lastMethod === "google"}
-						onClick={() => handleOAuthSignIn("google")}
-					/>
-				</div>
-				<div className="mt-4 text-center text-sm">
-					Don&apos;t have an account?{" "}
-					<Link to="/signup" className="underline">
-						Sign up
-					</Link>
-				</div>
-			</CardContent>
-		</Card>
+					<SeparatorWithText text="Or continue with" />
+					<div className="flex gap-2">
+						<OAuthButton
+							icon={FcGoogle}
+							label="Sign in with Google"
+							text="Google"
+							lastMethod={lastMethod === "google"}
+							onClick={() => handleOAuthSignIn("google")}
+						/>
+					</div>
+					<div className="mt-4 text-center text-sm">
+						Don&apos;t have an account?{" "}
+						<Link to="/signup" className="underline">
+							Sign up
+						</Link>
+					</div>
+				</CardContent>
+			</Card>
+			<TwoFactorDialog
+				open={showDialog}
+				onOpenChange={setShowDialog}
+				onVerify={handleVerifyTotp}
+			/>
+		</>
 	);
 }
