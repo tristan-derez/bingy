02449348b922c0b5fc60c1369605c839bb6284b9
Tristan <tristan.derez@gmail.com>
2025-11-04 16:38:19 +0100

    refactor(back): use zValidator instead of manually validating query/param

--- apps/backend/src/web/routes/person.ts
+++ apps/backend/src/web/routes/person.ts
@@ -1,89 +1,36 @@
-import { type Context, Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
 import { logger } from "#lib/logger";
-import {
-	serveBadRequest,
-	serveInternalServerError,
-} from "#lib/responses/error";
+import { serveInternalServerError } from "#lib/responses/error";
 import { serveData } from "#lib/responses/resp";
 import { tmdbClient } from "#lib/tmdb/tmdb.client";
+import {
+	idParamSchema,
+	languageQuerySchema,
+	paginationQuerySchema,
+} from "#schemas/queries-params";
 
 const personRoutes = new Hono();
 
-personRoutes.get("/:id", async (c: Context) => {
-	const id = Number(c.req.param("id"));
-	const language = c.req.query("language") ?? "en-US";
+personRoutes.get(
+	"/popular",
+	zValidator("query", paginationQuerySchema),
+	async (c) => {
+		const { language, page } = c.req.valid("query");
 
-	if (Number.isNaN(id)) return serveBadRequest(c, "Invalid person ID");
+		try {
+			const personPopular = await tmdbClient.get("/person/popular", {
+				query: { language, page },
+			});
+			return serveData(c, personPopular);
+		} catch (error) {
+			logger.error(error);
+			return serveInternalServerError(c, error);
+		}
+	},
+);
 
-	try {
-		const person = await tmdbClient.get("/person/{person_id}", {
-			query: { language },
-			path: { person_id: id },
-		});
-		return serveData(c, person);
-	} catch (error) {
-		logger.error(error);
-		return serveInternalServerError(c, error);
-	}
-});
-
-personRoutes.get("/:id/combined_credits", async (c: Context) => {
-	const id = Number(c.req.param("id"));
-	const language = c.req.query("language") ?? "en-US";
-
-	if (Number.isNaN(id)) return serveBadRequest(c, "Invalid person ID");
-
-	try {
-		const personCombinedCredits = await tmdbClient.get(
-			"/person/{person_id}/combined_credits",
-			{
-				query: { language },
-				path: { person_id: id },
-			},
-		);
-		return serveData(c, personCombinedCredits);
-	} catch (error) {
-		logger.error(error);
-		return serveInternalServerError(c, error);
-	}
-});
-
-personRoutes.get("/:id/external_ids", async (c: Context) => {
-	const id = Number(c.req.param("id"));
-
-	if (Number.isNaN(id)) return serveBadRequest(c, "Invalid person ID");
-
-	try {
-		const personExternalIds = await tmdbClient.get(
-			"/person/{person_id}/external_ids",
-			{
-				path: { person_id: id },
-			},
-		);
-		return serveData(c, personExternalIds);
-	} catch (error) {
-		logger.error(error);
-		return serveInternalServerError(c, error);
-	}
-});
-
-personRoutes.get("/:id/images", async (c: Context) => {
-	const id = Number(c.req.param("id"));
-
-	if (Number.isNaN(id)) return serveBadRequest(c, "Invalid person ID");
-
-	try {
-		const personImages = await tmdbClient.get("/person/{person_id}/images", {
-			path: { person_id: id },
-		});
-		return serveData(c, personImages);
-	} catch (error) {
-		logger.error(error);
-		return serveInternalServerError(c, error);
-	}
-});
-
-personRoutes.get("/latest", async (c: Context) => {
+personRoutes.get("/latest", async (c) => {
 	try {
 		const person = await tmdbClient.get("/person/latest");
 		return serveData(c, person);
@@ -93,80 +40,157 @@ personRoutes.get("/latest", async (c: Context) => {
 	}
 });
 
-personRoutes.get("/:id/movie_credits", async (c: Context) => {
-	const id = Number(c.req.param("id"));
-	const language = c.req.query("language") ?? "en-US";
+personRoutes.get(
+	"/:id",
+	zValidator("param", idParamSchema),
+	zValidator("query", languageQuerySchema),
+	async (c) => {
+		const { id } = c.req.valid("param");
+		const { language } = c.req.valid("query");
 
-	if (Number.isNaN(id)) return serveBadRequest(c, "Invalid person ID");
-
-	try {
-		const personMovieCredits = await tmdbClient.get(
-			"/person/{person_id}/movie_credits",
-			{
+		try {
+			const person = await tmdbClient.get("/person/{person_id}", {
 				query: { language },
 				path: { person_id: id },
-			},
-		);
-		return serveData(c, personMovieCredits);
-	} catch (error) {
-		logger.error(error);
-		return serveInternalServerError(c, error);
-	}
-});
+			});
+			return serveData(c, person);
+		} catch (error) {
+			logger.error(error);
+			return serveInternalServerError(c, error);
+		}
+	},
+);
 
-personRoutes.get("/:id/tv_credits", async (c: Context) => {
-	const id = Number(c.req.param("id"));
-	const language = c.req.query("language") ?? "en-US";
+personRoutes.get(
+	"/:id/combined_credits",
+	zValidator("param", idParamSchema),
+	zValidator("query", languageQuerySchema),
+	async (c) => {
+		const { id } = c.req.valid("param");
+		const { language } = c.req.valid("query");
 
-	if (Number.isNaN(id)) return serveBadRequest(c, "Invalid person ID");
+		try {
+			const personCombinedCredits = await tmdbClient.get(
+				"/person/{person_id}/combined_credits",
+				{
+					query: { language },
+					path: { person_id: id },
+				},
+			);
+			return serveData(c, personCombinedCredits);
+		} catch (error) {
+			logger.error(error);
+			return serveInternalServerError(c, error);
+		}
+	},
+);
 
-	try {
-		const personTvCredits = await tmdbClient.get(
-			"/person/{person_id}/tv_credits",
-			{
-				query: { language },
+personRoutes.get(
+	"/:id/external_ids",
+	zValidator("param", idParamSchema),
+	async (c) => {
+		const { id } = c.req.valid("param");
+
+		try {
+			const personExternalIds = await tmdbClient.get(
+				"/person/{person_id}/external_ids",
+				{
+					path: { person_id: id },
+				},
+			);
+			return serveData(c, personExternalIds);
+		} catch (error) {
+			logger.error(error);
+			return serveInternalServerError(c, error);
+		}
+	},
+);
+
+personRoutes.get(
+	"/:id/images",
+	zValidator("param", idParamSchema),
+	async (c) => {
+		const { id } = c.req.valid("param");
+
+		try {
+			const personImages = await tmdbClient.get("/person/{person_id}/images", {
 				path: { person_id: id },
-			},
-		);
-		return serveData(c, personTvCredits);
-	} catch (error) {
-		logger.error(error);
-		return serveInternalServerError(c, error);
-	}
-});
+			});
+			return serveData(c, personImages);
+		} catch (error) {
+			logger.error(error);
+			return serveInternalServerError(c, error);
+		}
+	},
+);
 
-personRoutes.get("/:id/translations", async (c: Context) => {
-	const id = Number(c.req.param("id"));
+personRoutes.get(
+	"/:id/movie_credits",
+	zValidator("param", idParamSchema),
+	zValidator("query", languageQuerySchema),
+	async (c) => {
+		const { id } = c.req.valid("param");
+		const { language } = c.req.valid("query");
 
-	if (Number.isNaN(id)) return serveBadRequest(c, "Invalid person ID");
+		try {
+			const personMovieCredits = await tmdbClient.get(
+				"/person/{person_id}/movie_credits",
+				{
+					query: { language },
+					path: { person_id: id },
+				},
+			);
+			return serveData(c, personMovieCredits);
+		} catch (error) {
+			logger.error(error);
+			return serveInternalServerError(c, error);
+		}
+	},
+);
 
-	try {
-		const personTranslations = await tmdbClient.get(
-			"/person/{person_id}/translations",
-			{
-				path: { person_id: id },
-			},
-		);
-		return serveData(c, personTranslations);
-	} catch (error) {
-		logger.error(error);
-		return serveInternalServerError(c, error);
-	}
-});
+personRoutes.get(
+	"/:id/tv_credits",
+	zValidator("param", idParamSchema),
+	zValidator("query", languageQuerySchema),
+	async (c) => {
+		const { id } = c.req.valid("param");
+		const { language } = c.req.valid("query");
 
-personRoutes.get("/popular", async (c: Context) => {
-	const language = c.req.query("language") ?? "en-US";
-	const page = Number(c.req.query("page")) ?? 1;
+		try {
+			const personTvCredits = await tmdbClient.get(
+				"/person/{person_id}/tv_credits",
+				{
+					query: { language },
+					path: { person_id: id },
+				},
+			);
+			return serveData(c, personTvCredits);
+		} catch (error) {
+			logger.error(error);
+			return serveInternalServerError(c, error);
+		}
+	},
+);
 
-	try {
-		const personPopular = await tmdbClient.get("/person/popular", {
-			query: { language, page },
-		});
-		return serveData(c, personPopular);
-	} catch (error) {
-		logger.error(error);
-		return serveInternalServerError(c, error);
-	}
-});
+personRoutes.get(
+	"/:id/translations",
+	zValidator("param", idParamSchema),
+	async (c) => {
+		const { id } = c.req.valid("param");
+
+		try {
+			const personTranslations = await tmdbClient.get(
+				"/person/{person_id}/translations",
+				{
+					path: { person_id: id },
+				},
+			);
+			return serveData(c, personTranslations);
+		} catch (error) {
+			logger.error(error);
+			return serveInternalServerError(c, error);
+		}
+	},
+);
 
 export default personRoutes;
