bd22c66a1ee5f4cc170867dd6f865915b918a9b0
Tristan <tristan.derez@gmail.com>
2025-11-04 22:49:26 +0100

    fix(back): change path

--- apps/backend/src/web/routes/genre.ts
+++ apps/backend/src/web/routes/genre.ts
@@ -8,32 +8,40 @@ import { languageQuerySchema } from "#web/validators/query-param";
 
 const genreRoutes = new Hono();
 
-genreRoutes.get("/:id", zValidator("query", languageQuerySchema), async (c) => {
-	const { language } = c.req.valid("query");
+genreRoutes.get(
+	"/movie/list",
+	zValidator("query", languageQuerySchema),
+	async (c) => {
+		const { language } = c.req.valid("query");
 
-	try {
-		const results = await tmdbClient.get("/genre/movie/list", {
-			query: { language },
-		});
-		return serveData(c, results);
-	} catch (error) {
-		logger.error(error);
-		return serveInternalServerError(c, error);
-	}
-});
+		try {
+			const results = await tmdbClient.get("/genre/movie/list", {
+				query: { language },
+			});
+			return serveData(c, results);
+		} catch (error) {
+			logger.error(error);
+			return serveInternalServerError(c, error);
+		}
+	},
+);
 
-genreRoutes.get("/:id", zValidator("query", languageQuerySchema), async (c) => {
-	const { language } = c.req.valid("query");
+genreRoutes.get(
+	"/tv/list",
+	zValidator("query", languageQuerySchema),
+	async (c) => {
+		const { language } = c.req.valid("query");
 
-	try {
-		const results = await tmdbClient.get("/genre/tv/list", {
-			query: { language },
-		});
-		return serveData(c, results);
-	} catch (error) {
-		logger.error(error);
-		return serveInternalServerError(c, error);
-	}
-});
+		try {
+			const results = await tmdbClient.get("/genre/tv/list", {
+				query: { language },
+			});
+			return serveData(c, results);
+		} catch (error) {
+			logger.error(error);
+			return serveInternalServerError(c, error);
+		}
+	},
+);
 
 export default genreRoutes;
