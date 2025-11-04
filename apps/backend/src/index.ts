a8f205ec63147e748edbd474ef219cdfb0fe2d47
Tristan <tristan.derez@gmail.com>
2025-11-04 17:12:07 +0100

    feat(back): add genre routes

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -13,6 +13,7 @@ import { serveInternalServerError } from "#lib/responses/error";
 import { sessionMiddleware } from "#web/middlewares/session";
 import authRoutes from "#web/routes/auth";
 import discoverRoutes from "#web/routes/discover";
+import genreRoutes from "#web/routes/genre";
 import movieRoutes from "#web/routes/movie";
 import networkRoutes from "#web/routes/network";
 import personRoutes from "#web/routes/person";
@@ -64,6 +65,7 @@ api.route("/search", searchRoutes);
 api.route("/trending", trendingRoutes);
 api.route("network", networkRoutes);
 api.route("/discover", discoverRoutes);
+api.route("/genre", genreRoutes);
 app.route("/api", api);
 
 app.onError((err, c) => {
---
+++ apps/backend/src/web/routes/genre.ts
@@ -0,0 +1,37 @@
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { logger } from "#lib/logger";
+import { serveInternalServerError } from "#lib/responses/error";
+import { serveData } from "#lib/responses/resp";
+import { tmdbClient } from "#lib/tmdb/tmdb.client";
+import { languageQuerySchema } from "#web/validators/query-param";
+
+const genreRoutes = new Hono();
+
+genreRoutes.get("/:id", zValidator("query", languageQuerySchema), async (c) => {
+	const { language } = c.req.valid("query");
+	try {
+		const results = await tmdbClient.get("/genre/movie/list", {
+			query: { language },
+		});
+		return serveData(c, results);
+	} catch (error) {
+		logger.error(error);
+		return serveInternalServerError(c, error);
+	}
+});
+
+genreRoutes.get("/:id", zValidator("query", languageQuerySchema), async (c) => {
+	const { language } = c.req.valid("query");
+	try {
+		const results = await tmdbClient.get("/genre/tv/list", {
+			query: { language },
+		});
+		return serveData(c, results);
+	} catch (error) {
+		logger.error(error);
+		return serveInternalServerError(c, error);
+	}
+});
+
+export default genreRoutes;
