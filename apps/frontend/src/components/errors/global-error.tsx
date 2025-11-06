96c5b8c40695cba92ddee33dab8a4be6b45c6ba8
Tristan <tristan.derez@gmail.com>
2025-11-06 23:14:18 +0100

    feat(front): add LightRays to GlobalError

--- apps/frontend/src/components/errors/global-error.tsx
+++ apps/frontend/src/components/errors/global-error.tsx
@@ -5,6 +5,7 @@ import {
 	CardDescription,
 	CardTitle,
 } from "@/components/ui/card";
+import { LightRays } from "../ui/light-rays";
 
 type GlobalErrorProps = {
 	error: unknown;
@@ -15,7 +16,7 @@ export function GlobalError({ error }: GlobalErrorProps) {
 		error instanceof Error ? error.message : "Unknown error occurred";
 
 	return (
-		<div className="h-screen w-screen flex items-center justify-center bg-purple-950/10 bg-[radial-gradient(ellipse_20%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] px-4">
+		<div className="h-screen w-screen flex items-center justify-center p-4">
 			<Card className="max-w-md w-full shadow-lg p-2">
 				<CardContent className="flex flex-col items-center text-center p-4 gap-4">
 					<CardTitle className="text-2xl text-red-500">Oops!</CardTitle>
@@ -29,6 +30,7 @@ export function GlobalError({ error }: GlobalErrorProps) {
 					</Button>
 				</CardContent>
 			</Card>
+			<LightRays />
 		</div>
 	);
 }
