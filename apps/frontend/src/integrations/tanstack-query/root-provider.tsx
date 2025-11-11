68562945328242ed37723cfff32334fa169a0b2b
Tristan <tristan.derez@gmail.com>
2025-11-11 23:01:51 +0100

    refactor(front): remove comment

--- apps/frontend/src/integrations/tanstack-query/root-provider.tsx
+++ apps/frontend/src/integrations/tanstack-query/root-provider.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { ReactNode } from "react";
 
-// single instance
 export const queryClient = new QueryClient();
 
 export function Provider({ children }: { children: ReactNode }) {
