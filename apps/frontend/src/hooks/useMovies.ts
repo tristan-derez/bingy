0c88fc2f6ad8082a6f47faf6c49aabbc324479d0
Tristan <tristan.derez@gmail.com>
2025-11-11 23:01:35 +0100

    feat(front): create basic hooks

---
+++ apps/frontend/src/hooks/useMovies.ts
@@ -0,0 +1,56 @@
+import { useQuery } from "@tanstack/react-query";
+import {
+	fetchMovie,
+	fetchMovieCredits,
+	fetchMovies,
+	type MovieParams,
+} from "@/api/movies";
+
+export function useLatestMovie() {
+	return useQuery({
+		queryKey: ["movies", "latest"],
+		queryFn: () => fetchMovies("latest"),
+	});
+}
+
+export function useNowPlayingMovies(params?: MovieParams) {
+	return useQuery({
+		queryKey: ["movies", "now_playing", params],
+		queryFn: () => fetchMovies("now_playing", params),
+	});
+}
+
+export function usePopularMovies(params?: MovieParams) {
+	return useQuery({
+		queryKey: ["movies", "popular", params],
+		queryFn: () => fetchMovies("popular", params),
+	});
+}
+
+export function useTopRatedMovies(params?: MovieParams) {
+	return useQuery({
+		queryKey: ["movies", "top_rated", params],
+		queryFn: () => fetchMovies("top_rated", params),
+	});
+}
+
+export function useUpcomingMovies(params?: MovieParams) {
+	return useQuery({
+		queryKey: ["movies", "upcoming", params],
+		queryFn: () => fetchMovies("upcoming", params),
+	});
+}
+
+export function useMovie(id: string, language?: string) {
+	return useQuery({
+		queryKey: ["movies", id, { language }],
+		queryFn: () => fetchMovie(id, { language }),
+	});
+}
+
+export function useMovieCredits(id: string, language?: string) {
+	return useQuery({
+		queryKey: ["movies", id, "credits", { language }],
+		queryFn: () => fetchMovieCredits(id, language),
+	});
+}
