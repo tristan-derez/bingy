d5ed927939d1b288bc1ab5324cda2cf53b5852b9
Tristan <tristan.derez@gmail.com>
2025-11-19 21:53:03 +0100

    refactor(front): add movie-carousel in now playing

--- apps/frontend/src/components/movies/now-playing.tsx
+++ apps/frontend/src/components/movies/now-playing.tsx
@@ -1,28 +1,10 @@
-import useEmblaCarousel from "embla-carousel-react";
 import { toast } from "sonner";
-import {
-	NextButton,
-	PrevButton,
-	usePrevNextButtons,
-} from "@/components/ui/embla/embla-carousel-arrow-buttons";
 import { useNowPlayingMovies } from "@/hooks/useMovies";
-import type { Movie } from "@/types/movie";
 import { LoadingSection } from "../loading/loading-section";
-import { MovieCard } from "./movie-card";
+import { MovieCarousel } from "./movie-carousel";
 
 export const NowPlayingMovies = () => {
 	const { data, isLoading, error } = useNowPlayingMovies();
-	const [emblaRef, emblaApi] = useEmblaCarousel({
-		dragFree: true,
-		align: "start",
-	});
-
-	const {
-		prevBtnDisabled,
-		nextBtnDisabled,
-		onPrevButtonClick,
-		onNextButtonClick,
-	} = usePrevNextButtons(emblaApi);
 
 	if (isLoading) {
 		return <LoadingSection title="In Theaters Now" />;
@@ -33,29 +15,5 @@ export const NowPlayingMovies = () => {
 		return null;
 	}
 
-	if (!data || !data.results?.length) {
-		return (
-			<div>
-				<p>No movies found</p>
-			</div>
-		);
-	}
-	return (
-		<section className="flex flex-col gap-4">
-			<h2 className="text-xl font-semibold">In Theaters Now</h2>
-			<div className="overflow-hidden hover:cursor-grab" ref={emblaRef}>
-				<div className="flex gap-4">
-					{data.results.map((movie: Movie) => (
-						<div key={movie.id} className="min-w-80 md:min-w-60">
-							<MovieCard movie={movie} />
-						</div>
-					))}
-				</div>
-			</div>
-			<div className="flex items-center gap-2">
-				<PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-				<NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
-			</div>
-		</section>
-	);
+	return <MovieCarousel title="In Theaters Now" movies={data?.results ?? []} />;
 };
