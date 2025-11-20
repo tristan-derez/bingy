a88b6a1fc89339f51572da8bd8c1f98f7a5b3177
Tristan <tristan.derez@gmail.com>
2025-11-20 16:47:20 +0100

    feat(front): create person card

---
+++ apps/frontend/src/components/person/person-card.tsx
@@ -0,0 +1,48 @@
+import fallbackPoster from "@/assets/movie-placeholder.jpg";
+import {
+	Card,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import type { CastMember } from "../movies/movie-details";
+
+interface PersonCardProps {
+	person: CastMember;
+}
+
+export const PersonCard = ({ person }: PersonCardProps) => {
+	const imageUrl = person.profile_path
+		? `https://image.tmdb.org/t/p/w200${person.profile_path}`
+		: fallbackPoster;
+
+	return (
+		<Card className="w-full max-w-30 lg:max-w-60 min-h-[250px] overflow-hidden pt-0">
+			<div className="w-full h-auto">
+				<img
+					src={imageUrl}
+					alt={person.name}
+					onError={(e) => {
+						if (e.currentTarget.src !== fallbackPoster)
+							e.currentTarget.src = fallbackPoster;
+					}}
+					className="h-full w-full object-contain"
+				/>
+			</div>
+			<CardHeader className="flex-grow justify-between py-2 px-3">
+				<CardTitle
+					className="text-base leading-tight line-clamp-1"
+					title={person.name}
+				>
+					{person.name}
+				</CardTitle>
+				<CardDescription
+					className="text-sm leading-tight line-clamp-1 mt-1"
+					title={person.character}
+				>
+					{person.character}
+				</CardDescription>
+			</CardHeader>
+		</Card>
+	);
+};
