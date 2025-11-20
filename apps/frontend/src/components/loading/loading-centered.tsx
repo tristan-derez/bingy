f90f58061f3000acdb10fa47a10cf2d62e342216
Tristan <tristan.derez@gmail.com>
2025-11-20 14:47:59 +0100

    feat(front): add loading centered

---
+++ apps/frontend/src/components/loading/loading-centered.tsx
@@ -0,0 +1,10 @@
+import { CenteredLayout } from "../layout/centered-layout";
+import { LoaderOne } from "../ui/loader";
+
+export function LoadingCentered() {
+	return (
+		<CenteredLayout>
+			<LoaderOne />
+		</CenteredLayout>
+	);
+}
