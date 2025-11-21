c18971e06189d1b1b38d61231fa9fd04c49249c7
Tristan <tristan.derez@gmail.com>
2025-11-21 11:36:26 +0100

    feat: add carousel gradient (#34)

--- apps/frontend/src/components/person/person-carousel.tsx
+++ apps/frontend/src/components/person/person-carousel.tsx
@@ -5,6 +5,10 @@ import {
 	usePrevNextButtons,
 } from "@/components/ui/embla/embla-carousel-arrow-buttons";
 import type { CastMember } from "../movies/movie-details";
+import {
+	CarouselGradient,
+	useCarouselGradient,
+} from "../ui/embla/embla-carousel-gradient";
 import { PersonCard } from "./person-card";
 
 interface PersonCarouselProps {
@@ -24,6 +28,8 @@ export const PersonCarousel = ({ people }: PersonCarouselProps) => {
 		onNextButtonClick,
 	} = usePrevNextButtons(emblaApi);
 
+	const { showGradient } = useCarouselGradient(emblaApi);
+
 	if (!people.length) {
 		return (
 			<div>
@@ -34,14 +40,17 @@ export const PersonCarousel = ({ people }: PersonCarouselProps) => {
 
 	return (
 		<section className="flex flex-col gap-1 lg:gap-2">
-			<div className="overflow-hidden hover:cursor-grab" ref={emblaRef}>
-				<div className="flex gap-1 lg:gap-4">
-					{people.map((person) => (
-						<div key={person.id} className="min-w-30 lg:min-w-60">
-							<PersonCard key={person.id} person={person} />
-						</div>
-					))}
+			<div className="relative">
+				<div className="overflow-hidden hover:cursor-grab" ref={emblaRef}>
+					<div className="flex gap-1 lg:gap-4">
+						{people.map((person) => (
+							<div key={person.id} className="min-w-30 lg:min-w-60">
+								<PersonCard person={person} />
+							</div>
+						))}
+					</div>
 				</div>
+				<CarouselGradient show={showGradient} />
 			</div>
 			<div className="flex items-center gap-2">
 				<PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
