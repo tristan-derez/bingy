733e6c8136e749de22f8010f5b2c84f042a75e3b
Tristan <tristan.derez@gmail.com>
2025-11-22 19:59:46 +0100

    feat(front): add types

---
+++ apps/frontend/src/types/episode.ts
@@ -0,0 +1,26 @@
+import type { Pretty } from "./generic";
+import type { CastMember, CrewMember } from "./person";
+
+export type EpisodeWithCrewAndGuestStars = Pretty<
+	Omit<Episode, "media_type"> & {
+		crew: CrewMember[];
+		guest_star: CastMember[];
+	}
+>;
+
+type Episode = Pretty<{
+	id: number;
+	name: string;
+	overview: string;
+	media_type: "tv_episode";
+	vote_average: number;
+	vote_count: string;
+	air_date: string | null;
+	episode_number: number;
+	episode_type: string;
+	production_code: string;
+	runtime: number | null;
+	season_number: number;
+	show_id: number;
+	still_path: string | null;
+}>;
--- apps/frontend/src/types/person.ts
+++ apps/frontend/src/types/person.ts
@@ -84,3 +84,32 @@ type Job = Pretty<{
 	job: string;
 	episode_count: number;
 }>;
+
+export type CrewMember = {
+	adult: boolean;
+	gender: number | null;
+	id: number;
+	known_for_department: string;
+	name: string;
+	original_name: string;
+	popularity: number;
+	profile_path: string | null;
+	credit_id: string;
+	department: string;
+	job: string;
+};
+
+export type CastMember = {
+	adult: boolean;
+	gender: number | null;
+	id: number;
+	known_for_department: string;
+	name: string;
+	original_name: string;
+	popularity: number;
+	profile_path: string | null;
+	cast_id: number;
+	character: string;
+	credit_id: string;
+	order: number;
+};
