38e49c21d37daab286742a3f7ea332fc317683b4
Tristan <tristan.derez@gmail.com>
2025-11-26 13:11:07 +0100

    feat(front): add types for medias and person

--- apps/frontend/src/types/media.ts
+++ apps/frontend/src/types/media.ts
@@ -28,7 +28,7 @@ export type MovieMedia = Pretty<
 	}
 >;
 
-type TvMedia = Pretty<
+export type TvMedia = Pretty<
 	BaseMedia & {
 		media_type: "tv";
 		name: string;
@@ -39,3 +39,46 @@ type TvMedia = Pretty<
 		seasons: Season[];
 	}
 >;
+
+export type MovieMediaWithCastCredits = Pretty<
+	MovieMedia & {
+		order: number;
+		character: string;
+		credit_id: string;
+	}
+>;
+
+export type TvMediaWithCastCredits = Pretty<
+	Omit<TvMedia, "seasons" | "origin_country" | "episodes"> & {
+		origin_country: string[];
+		credit_id: string;
+		department: string;
+		character: string;
+		first_credit_air_date: string;
+	}
+>;
+
+export type MediaWithCastCredits =
+	| MovieMediaWithCastCredits
+	| TvMediaWithCastCredits;
+
+export type MovieMediaWithCrewCredits = Pretty<
+	MovieMedia & {
+		department: string;
+		job: string;
+		credit_id: string;
+	}
+>;
+
+export type TvMediaWithCrewCredits = Pretty<
+	Omit<TvMedia, "seasons" | "origin_country" | "episodes"> & {
+		first_credit_air_date: string;
+		department: string;
+		credit_id: string;
+		origin_country: string[];
+	}
+>;
+
+export type MediaWithCrewCredits =
+	| MovieMediaWithCrewCredits
+	| TvMediaWithCrewCredits;
