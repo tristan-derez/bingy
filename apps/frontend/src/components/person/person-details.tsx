aa2b06aad67ad06b29e751372d4ed3fa471bee8e
Tristan <tristan.derez@gmail.com>
2025-11-26 15:06:18 +0100

    fix(front): remove unused import

--- apps/frontend/src/components/person/person-details.tsx
+++ apps/frontend/src/components/person/person-details.tsx
@@ -5,10 +5,7 @@ import type { PersonDetails } from "@/types/person";
 import { calculateAge } from "@/utils/calculate-age";
 import { formatDate } from "@/utils/format-date";
 import { getSocialUrls } from "@/utils/social-urls";
-import {
-	getSortedKnownForCredits,
-	sortKnownForCredits,
-} from "@/utils/sort-known-credits";
+import { sortKnownForCredits } from "@/utils/sort-known-credits";
 import { ResourceNotFound } from "../errors/resource-not-found";
 import { LoadingCentered } from "../loading/loading-centered";
 import { MediasCarousel } from "../medias/medias-carousel";
