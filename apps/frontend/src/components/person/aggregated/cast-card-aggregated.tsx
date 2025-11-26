62863dcd7abec90ef75464a2d8e44d998cdac1cf
Tristan <tristan.derez@gmail.com>
2025-11-26 10:07:44 +0100

    feat(front): add Link to person page

--- apps/frontend/src/components/person/aggregated/cast-card-aggregated.tsx
+++ apps/frontend/src/components/person/aggregated/cast-card-aggregated.tsx
@@ -67,7 +67,12 @@ export const CastCardAggregated = ({ person }: CastCardAggregatedProps) => {
 				<CardContent className="text-center pt-0 gap-4 flex flex-col">
 					<Separator />
 					<Button asChild>
-						<Link to="/">See more</Link>
+						<Link
+							to="/person/$personId"
+							params={{ personId: person.id.toString() }}
+						>
+							See more
+						</Link>
 					</Button>
 				</CardContent>
 			</Card>
