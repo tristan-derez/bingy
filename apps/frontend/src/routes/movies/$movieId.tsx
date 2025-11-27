0e52fef023681828cf99e0732532db76922fcce3
Tristan <tristan.derez@gmail.com>
2025-11-27 18:27:37 +0100

    refactor(front): use append_to_response to make 1 call instead of 4

--- apps/frontend/src/routes/movies/$movieId.tsx
+++ apps/frontend/src/routes/movies/$movieId.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute, useRouter } from "@tanstack/react-router";
-import type { Schemas } from "shared";
 import { MovieDetailView } from "@/components/movies/movie-details";
-import { useMovie, useMovieResource } from "@/hooks/useMovies";
+import { useMovie } from "@/hooks/useMovies";
 import { getRole } from "@/utils/excluded-jobs";
 import { getSocialUrls } from "@/utils/social-urls";
 
@@ -13,44 +12,37 @@ function MovieDetailsContainer() {
 	const router = useRouter();
 	const { movieId } = Route.useParams();
 
-	const { data: movie, isLoading, isError } = useMovie(Number(movieId));
-	const { data: credits } = useMovieResource<Schemas.MovieCredits>(
-		Number(movieId),
-		"credits",
-	);
-
-	const { data: socials } = useMovieResource<Schemas.MovieExternalIds>(
-		Number(movieId),
-		"external_ids",
-	);
-
-	const { data: watchProviders } = useMovieResource<Schemas.WatchProviders>(
-		Number(movieId),
-		"watch/providers",
-	);
+	const {
+		data: movie,
+		isLoading,
+		isError,
+	} = useMovie(Number(movieId), {
+		append_to_response: "credits,external_ids,watch/providers",
+	});
 
 	const crewWithRoles =
-		credits?.crew.reduce<Map<number, { name: string; roles: Set<string> }>>(
-			(map, person) => {
-				const role = getRole(person);
-				if (!role) return map;
+		movie?.credits?.crew.reduce<
+			Map<number, { name: string; roles: Set<string> }>
+		>((map, person) => {
+			const role = getRole(person);
+			if (!role) return map;
 
-				const existing = map.get(person.id);
-				if (existing) {
-					existing.roles.add(role);
-				} else {
-					map.set(person.id, { name: person.name, roles: new Set([role]) });
-				}
-				return map;
-			},
-			new Map(),
-		) ?? new Map();
+			const existing = map.get(person.id);
+			if (existing) {
+				existing.roles.add(role);
+			} else {
+				map.set(person.id, { name: person.name, roles: new Set([role]) });
+			}
+			return map;
+		}, new Map()) ?? new Map();
 
 	const crew = Array.from(crewWithRoles.values());
-	const cast = credits?.cast?.slice(0, 10) || [];
-
-	const socialUrls = socials ? getSocialUrls(socials) : {};
+	const cast = movie?.credits?.cast?.slice(0, 10) || [];
+	const socialUrls = movie?.external_ids
+		? getSocialUrls(movie.external_ids)
+		: {};
 	const collection = movie ? movie.belongs_to_collection : undefined;
+	const watchProviders = movie?.["watch/providers"];
 
 	return (
 		<MovieDetailView
