ccc3fbcabda855146ec1905fe04a19bb14a302f6
Tristan <tristan.derez@gmail.com>
2025-12-01 21:58:11 +0100

    refactor(front): move form into its own component

--- apps/frontend/src/schemas/search-form-schema.ts
+++ apps/frontend/src/schemas/search-form-schema.ts
@@ -4,5 +4,5 @@ export const searchFormSchema = z.object({
 	newQuery: z
 		.string()
 		.min(1, { message: "Search cannot be empty" })
-		.max(200, { message: "Search cannot exceed 200 characters" }),
+		.max(300, { message: "Search cannot exceed 300 characters" }),
 });
