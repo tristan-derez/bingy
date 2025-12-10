89346a69fe4a185c8994ae021fbd680a53e9ca14
Tristan <tristan.derez@gmail.com>
2025-12-10 14:26:10 +0100

    refactor(front): move root component into its own file

---
+++ apps/frontend/src/components/root-component.tsx
@@ -0,0 +1,31 @@
+import { TanstackDevtools } from "@tanstack/react-devtools";
+import { HeadContent, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
+import Header from "./Header";
+import { LightRays } from "./ui/light-rays";
+
+export function RootComponent() {
+	return (
+		<div>
+			<HeadContent />
+			<Header />
+			<div className="pt-20 min-h-svh flex flex-col items-center py-22 lg:py-32 px-2 md:px-6 lg:px-12 2xl:px-32">
+				<Outlet />
+			</div>
+			<TanstackDevtools
+				config={{
+					position: "bottom-left",
+				}}
+				plugins={[
+					{
+						name: "Tanstack Router",
+						render: <TanStackRouterDevtoolsPanel />,
+					},
+					TanStackQueryDevtools,
+				]}
+			/>
+			<LightRays />
+		</div>
+	);
+}
