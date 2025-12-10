ea8a37f0dd51804a444a8c07175485153d65d355
Tristan <tristan.derez@gmail.com>
2025-12-10 22:24:01 +0100

    fix(front): add overflow-x-hidden

--- apps/frontend/src/components/layout/centered-layout.tsx
+++ apps/frontend/src/components/layout/centered-layout.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 export function CenteredLayout({ children }: { children: ReactNode }) {
 	return (
-		<div className="flex flex-col items-center justify-center flex-1">
+		<div className="flex flex-col items-center justify-center flex-1 max-w-full overflow-x-hidden">
 			{children}
 		</div>
 	);
