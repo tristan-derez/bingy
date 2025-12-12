e341f8f70d626d1524f8ed5df226df508f8fbe8d
Tristan <tristan.derez@gmail.com>
2025-12-12 18:56:18 +0100

    fix(front): add language query to hook and fetch wrapper

--- apps/frontend/src/api/credit.ts
+++ apps/frontend/src/api/credit.ts
@@ -1,8 +1,14 @@
+import type { Pretty } from "shared";
 import { apiFetch } from "./fetcher";
 
-export const fetchCredit = async (credit_id: string) => {
+export const fetchCredit = async (credit_id: string, params?: CreditParams) => {
 	const res = await apiFetch(`/credit/${credit_id}`, {
 		method: "GET",
+		query: params,
 	});
 	return res.data;
 };
+
+export type CreditParams = Pretty<{
+	language?: string;
+}>;
