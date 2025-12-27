79dd1bdbd715395cbd7c90c08a6fa602005fffe4
Tristan <tristan.derez@gmail.com>
2025-12-27 15:47:52 +0100

    fix(front); redirect to correct url on mobile based on session

--- apps/frontend/src/components/header.tsx
+++ apps/frontend/src/components/header.tsx
@@ -81,7 +81,7 @@ export default function Header() {
 		},
 		{
 			name: m.header_link_profile(),
-			link: "/profile",
+			link: session ? `/user/${session?.user.name}` : `/signin`,
 			icon: <User />,
 		},
 	];
