6c1ec2ed8631b22736236642afe07bf818de9c4b
Tristan <tristan.derez@gmail.com>
2026-01-03 14:20:50 +0100

    fix(front): add rating field

--- apps/frontend/src/api/rating.ts
+++ apps/frontend/src/api/rating.ts
@@ -56,6 +56,7 @@ type TMDBMedia = {
 export type HistoryItem = TMDBMedia & {
 	mediaType: "movie" | "tv";
 	watchedAt: Date | null;
+	rating: string;
 	progress?: {
 		lastWatchedSeason: number;
 		lastWatchedEpisode: number;
