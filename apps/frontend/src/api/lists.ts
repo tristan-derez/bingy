d5f9824f920f956101e98acc8fc39b07a8306e04
Tristan <tristan.derez@gmail.com>
2026-01-20 20:02:48 +0100

    feat(front): add method to retrieve lists from an user

--- apps/frontend/src/api/lists.ts
+++ apps/frontend/src/api/lists.ts
@@ -1,7 +1,7 @@
 import type { Pretty } from "shared";
 import { apiFetch } from "./fetcher";
 
-type WatchlistResponse = {
+type WatchlistResponse = Pretty<{
 	data: Array<{
 		id: number;
 		title: string;
@@ -15,18 +15,37 @@ type WatchlistResponse = {
 	page: number;
 	total_pages: number;
 	total_results: number;
-};
+}>;
+
+type ListsResponse = Pretty<{
+	data: Array<{
+		updatedAt: Date | null;
+		createdAt: Date;
+		deletedAt: Date | null;
+		id: string;
+		userId: string;
+		name: string;
+		description: string | null;
+		visibility: "limited" | "private" | "public";
+	}>;
+	page: number;
+	total_pages: number;
+	total_results: number;
+}>;
 
 export const fetchWatchlist = async (
 	username: string,
 	page = 1,
 	language: string,
 	mediaType?: "movie" | "tv",
-): Promise<WatchlistResponse> => {
-	const res = await apiFetch(`/lists/${username}/watchlist`, {
-		method: "GET",
-		query: { page, language, ...(mediaType && { mediaType }) },
-	});
+) => {
+	const res = await apiFetch<WatchlistResponse>(
+		`/lists/${username}/watchlist`,
+		{
+			method: "GET",
+			query: { page, language, ...(mediaType && { mediaType }) },
+		},
+	);
 	return res;
 };
 
@@ -57,6 +76,54 @@ export const deleteMediaWatchlist = async (
 		method: "DELETE",
 	});
 };
+
+export const fetchLists = async (
+	username: string,
+	page = 1,
+	filter = "all",
+) => {
+	const res = await apiFetch<ListsResponse>(`/lists/${username}/lists`, {
+		method: "GET",
+		query: { page, filter },
+	});
+	return res;
+};
+
+export const postMediaList = async (payload: AddMediaToListPayload) => {
+	const res = await apiFetch(`/lists/items`, {
+		method: "POST",
+		body: payload,
+	});
+	return res.data;
+};
+
+export const deleteMediaList = async (payload: RemoveMediaFromListPayload) => {
+	await apiFetch(
+		`/lists/${payload.listId}/items/${payload.mediaType}/${payload.tmdbId}`,
+		{
+			method: "DELETE",
+		},
+	);
+};
+
+export type AddMediaToListPayload = Pretty<{
+	tmdbId: number;
+	mediaType: "movie" | "tv";
+	visibility: "private" | "public" | "limited";
+	listId?: string;
+	listName: string;
+	title?: string;
+	name?: string;
+}>;
+
+export type RemoveMediaFromListPayload = Pretty<{
+	listId: string;
+	tmdbId: number;
+	mediaType: "movie" | "tv";
+	listName: string;
+	title?: string;
+	name?: string;
+}>;
 
 export type AddMediaToWatchlistPayload = Pretty<{
 	tmdbId: number;
--- apps/frontend/src/hooks/useLists.ts
+++ apps/frontend/src/hooks/useLists.ts
@@ -1,11 +1,17 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { redirect } from "@tanstack/react-router";
 import { toast } from "sonner";
 import {
+	type AddMediaToListPayload,
 	type AddMediaToWatchlistPayload,
+	deleteMediaList,
 	deleteMediaWatchlist,
 	fetchCheckItemInWatchlist,
+	fetchLists,
 	fetchWatchlist,
+	postMediaList,
 	postMediaWatchlist,
+	type RemoveMediaFromListPayload,
 	type RemoveMediaFromWatchlistPayload,
 } from "@/api/lists";
 import { m } from "@/paraglide/messages";
@@ -69,6 +75,82 @@ export function useRemoveFromWatchlist() {
 		onError: (_, variables) => {
 			const title = variables.title || variables.name;
 			toast.error(m.toast_remove_watchlist_error({ media: `"${title}"` }));
+		},
+	});
+}
+
+export function useLists(username: string, page = 1, filter = "all") {
+	return useQuery({
+		queryKey: ["lists", "custom-list", username, page, filter],
+		queryFn: () => fetchLists(username, page, filter),
+		staleTime: 1000 * 60 * 50,
+	});
+}
+
+export function useAddMediaToList() {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationFn: async (payload: AddMediaToListPayload) => {
+			return postMediaList(payload);
+		},
+		onSuccess: (_, variables) => {
+			queryClient.invalidateQueries({
+				queryKey: ["lists", "list", variables.name],
+			});
+			const title = variables.title || variables.name;
+			toast.success(
+				m.toast_add_to_list_media_success({
+					media: `"${title}"`,
+					list: variables.listName,
+				}),
+				{
+					action: {
+						label: "See list",
+						onClick: () => redirect({ to: "/" }),
+					},
+				},
+			);
+		},
+		onError: (_, variables) => {
+			const title = variables.title || variables.name;
+			toast.error(
+				m.toast_add_to_list_media_error({
+					media: `"${title}"`,
+					list: variables.listName,
+				}),
+			);
+		},
+	});
+}
+
+export function useRemoveFromList() {
+	const queryClient = useQueryClient();
+
+	return useMutation({
+		mutationFn: async (payload: RemoveMediaFromListPayload) => {
+			return deleteMediaList(payload);
+		},
+		onSuccess: (_, variables) => {
+			queryClient.invalidateQueries({
+				queryKey: ["lists", "list", variables.listName],
+			});
+			const title = variables.title || variables.name;
+			toast.success(
+				m.toast_remove_from_list_media_success({
+					media: `"${title}"`,
+					list: variables.listName,
+				}),
+			);
+		},
+		onError: (_, variables) => {
+			const title = variables.title || variables.name;
+			toast.error(
+				m.toast_remove_from_list_media_error({
+					media: `"${title}"`,
+					list: variables.listName,
+				}),
+			);
 		},
 	});
 }
