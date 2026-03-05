abd3c56ae131677b98b11d8a8f4e80808fc1a285
Tristan <tristan.derez@gmail.com>
2026-03-05 20:57:17 +0100

    feat(back): add rating routes

---
+++ apps/backend/src/web/routes/rating.ts
@@ -0,0 +1,46 @@
+import { zValidator } from "@hono/zod-validator";
+import { and, eq } from "drizzle-orm";
+import { Hono } from "hono";
+import z from "zod";
+import { media } from "#db/schemas/list";
+import { db } from "#lib/database";
+
+const ratingRoutes = new Hono();
+
+ratingRoutes.get(
+	"/:mediaType/:tmdbId",
+	zValidator(
+		"param",
+		z.object({
+			mediaType: z.enum(["movie", "tv"]),
+			tmdbId: z.coerce.number(),
+		}),
+	),
+	async (c) => {
+		const { mediaType, tmdbId } = c.req.valid("param");
+
+		const mediaEntry = await db.query.media.findFirst({
+			where: and(eq(media.tmdbId, tmdbId), eq(media.mediaType, mediaType)),
+			columns: {
+				averageRating: true,
+				ratingCount: true,
+			},
+		});
+
+		if (!mediaEntry) {
+			return c.json(undefined, 200);
+		}
+
+		return c.json(
+			{
+				averageRating: mediaEntry.averageRating
+					? Number(mediaEntry.averageRating)
+					: null,
+				ratingCount: mediaEntry.ratingCount,
+			},
+			200,
+		);
+	},
+);
+
+export default ratingRoutes;
