ba5c3147dc6f2f430ed5bb0728ad23f43b4a4350
Tristan <tristan.derez@gmail.com>
2026-03-18 18:40:11 +0100

    feat(back): update schema for custom_lists title and description

---
+++ apps/backend/drizzle/0003_keen_thundra.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "custom_lists" ALTER COLUMN "name" SET DATA TYPE varchar(200);--> statement-breakpoint
+ALTER TABLE "custom_lists" ALTER COLUMN "slug" SET DATA TYPE varchar(300);--> statement-breakpoint
+ALTER TABLE "custom_lists" ALTER COLUMN "description" SET DATA TYPE varchar(2000);
---
+++ apps/backend/drizzle/0004_wonderful_red_hulk.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "list_items" ALTER COLUMN "note" SET DATA TYPE varchar(1000);
