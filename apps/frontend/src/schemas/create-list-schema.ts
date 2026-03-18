d170d7a25cd7907213a5960b28ac88df8e84657e
Tristan <tristan.derez@gmail.com>
2026-03-18 18:53:12 +0100

    fix: update schema message to reflect previous pr changes (#103)

--- apps/frontend/src/schemas/create-list-schema.ts
+++ apps/frontend/src/schemas/create-list-schema.ts
@@ -5,10 +5,10 @@ export const createListSchema = z.object({
 	name: z
 		.string()
 		.min(1, m.schema_create_list_name_min())
-		.max(200, m.schema_create_list_name_max()),
+		.max(200, m.schema_create_list_name_max({ maxCharsNumber: 200 })),
 	description: z
 		.string()
-		.max(2000, m.schema_create_list_description_max())
+		.max(2000, m.schema_create_list_description_max({ maxCharsNumber: 2000 }))
 		.optional(),
 	type: z.enum(["unranked", "ranked"]),
 	visibility: z.enum(["public", "limited", "private"]),
