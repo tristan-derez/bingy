cca4c87d97e4e26f5bcca634ebbde8f0e9609f7d
Tristan <tristan.derez@gmail.com>
2026-04-03 23:59:19 +0200

    fix(front): remove duplicate viewport meta tag

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -26,10 +26,6 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 				charSet: "utf-8",
 			},
 			{
-				name: "viewport",
-				content: "width=device-width, initial-scale=1",
-			},
-			{
 				title: "Bingy",
 			},
 			{
