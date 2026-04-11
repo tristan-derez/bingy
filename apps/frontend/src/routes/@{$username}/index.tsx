93815d3241f6a0f82f36bf5900b12edd6ea58592
Tristan <tristan.derez@gmail.com>
2026-04-11 23:56:14 +0200

    fix(front): comment out profile ratings and profile activity

--- apps/frontend/src/routes/@{$username}/index.tsx
+++ apps/frontend/src/routes/@{$username}/index.tsx
@@ -1,9 +1,7 @@
 import { createFileRoute, useRouteContext } from "@tanstack/react-router";
 import { useAtomValue } from "jotai";
-import { ProfileActivitySection } from "@/components/profile/profile-activity-section";
 import { ProfileFavoritesSection } from "@/components/profile/profile-favorites-section";
 import { ProfileListsSection } from "@/components/profile/profile-lists-section";
-import { ProfileRatingsSection } from "@/components/profile/profile-ratings-section";
 import { ProfileWatchlistSection } from "@/components/profile/profile-watchlist-section";
 import { localeRegionAtom } from "@/lib/atoms/region";
 import { isOwnProfile } from "@/utils/utils";
@@ -38,12 +36,12 @@ function ProfilePage() {
 				/>
 			</div>
 			<div className="flex flex-col gap-6 flex-1 max-w-full lg:max-w-3/9">
-				<ProfileRatingsSection username={username} />
-				<ProfileActivitySection
+				{/* <ProfileRatingsSection username={username} /> */}
+				{/* <ProfileActivitySection
 					username={username}
 					isOwnProfile={ownProfile}
 					localeRegion={localeRegion}
-				/>
+				/> */}
 			</div>
 		</div>
 	);
