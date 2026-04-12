c086b2f6063dbdb766b6e6b27d1514139f0bb83a
Tristan <tristan.derez@gmail.com>
2026-04-12 22:47:36 +0200

    feat(front): add search movie and search tv hooks and api calls

--- apps/frontend/src/api/search.ts
+++ apps/frontend/src/api/search.ts
@@ -14,6 +14,31 @@ export const fetchSearchMulti = async (
 	return res.data;
 };
 
+export const fetchSearchMovie = async (
+	query: string,
+	options?: SearchOptions,
+) => {
+	const { page = 1, language = "en-US" } = options ?? {};
+
+	const res = await apiFetch(`/search/movie`, {
+		method: "GET",
+		query: { query, page, language },
+	});
+
+	return res.data;
+};
+
+export const fetchSearchTv = async (query: string, options?: SearchOptions) => {
+	const { page = 1, language = "en-US" } = options ?? {};
+
+	const res = await apiFetch(`/search/tv`, {
+		method: "GET",
+		query: { query, page, language },
+	});
+
+	return res.data;
+};
+
 interface SearchOptions {
 	page?: number;
 	language?: string;
--- apps/frontend/src/hooks/useSearch.ts
+++ apps/frontend/src/hooks/useSearch.ts
@@ -1,6 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
-import { fetchSearchMulti } from "@/api/search";
+import {
+	fetchSearchMovie,
+	fetchSearchMulti,
+	fetchSearchTv,
+} from "@/api/search";
 
 export function useSearchQuery<T>(query: string, { language = "en-US" }) {
 	const [debouncedQuery, setDebouncedQuery] = useState(query);
@@ -16,6 +20,44 @@ export function useSearchQuery<T>(query: string, { language = "en-US" }) {
 	return useQuery<T>({
 		queryKey: ["search", "multi", debouncedQuery, language],
 		queryFn: () => fetchSearchMulti(debouncedQuery, { language }),
+		enabled: debouncedQuery.length > 0,
+		staleTime: 5 * 60 * 1000,
+	});
+}
+
+export function useSearchMovieQuery<T>(query: string, { language = "en-US" }) {
+	const [debouncedQuery, setDebouncedQuery] = useState(query);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setDebouncedQuery(query);
+		}, 500);
+
+		return () => clearTimeout(timer);
+	}, [query]);
+
+	return useQuery<T>({
+		queryKey: ["search", "movie", debouncedQuery, language],
+		queryFn: () => fetchSearchMovie(debouncedQuery, { language }),
+		enabled: debouncedQuery.length > 0,
+		staleTime: 5 * 60 * 1000,
+	});
+}
+
+export function useSearchTvQuery<T>(query: string, { language = "en-US" }) {
+	const [debouncedQuery, setDebouncedQuery] = useState(query);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setDebouncedQuery(query);
+		}, 500);
+
+		return () => clearTimeout(timer);
+	}, [query]);
+
+	return useQuery<T>({
+		queryKey: ["search", "tv", debouncedQuery, language],
+		queryFn: () => fetchSearchTv(debouncedQuery, { language }),
 		enabled: debouncedQuery.length > 0,
 		staleTime: 5 * 60 * 1000,
 	});
