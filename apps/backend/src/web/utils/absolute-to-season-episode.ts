8ab5e3ba430f480c07ba74f0b899b3b82a3558ff
Tristan <tristan.derez@gmail.com>
2026-04-21 22:13:03 +0200

    feat(back): add getValidSeason function

--- apps/backend/src/web/utils/absolute-to-season-episode.ts
+++ apps/backend/src/web/utils/absolute-to-season-episode.ts
@@ -3,21 +3,30 @@ interface Season {
 	episode_count: number;
 }
 
+/**
+ *
+ * @param seasons
+ * @returns seasons without the special season (season 0) and order them by season number
+ */
+export function getValidSeason(seasons: Season[]): Season[] {
+	return seasons
+		.filter((s) => s.season_number > 0)
+		.sort((a, b) => a.season_number - b.season_number);
+}
+
 export function convertAbsoluteToSeasonEpisode(
 	absoluteEpisode: number,
 	seasons: Season[],
-): { season: number; episode: number } {
-	const validSeasons = seasons
-		.filter((s) => s.season_number > 0)
-		.sort((a, b) => a.season_number - b.season_number);
+): Season {
+	const validSeasons = getValidSeason(seasons);
 
 	let remaining = absoluteEpisode;
 
 	for (const season of validSeasons) {
 		if (remaining <= season.episode_count) {
 			return {
-				season: season.season_number,
-				episode: remaining,
+				season_number: season.season_number,
+				episode_count: remaining,
 			};
 		}
 		remaining -= season.episode_count;
